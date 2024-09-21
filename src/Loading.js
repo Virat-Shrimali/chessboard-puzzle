ebeede997d874a5f8fb3304b6cef41766e7edd1c
Virat Shrimali <viratns05@gmail.com>
2024-09-21 23:28:50 +0530

    progress bar

--- src/Loading.js
+++ src/Loading.js
@@ -1,12 +1,15 @@
 // Loading.js
 import React from 'react';
-import './Loading.css'; // Custom styles for spinner
+import './Loading.css'; // Custom styles
 
 const Loading = () => {
   return (
     <div className="loading-container">
       <div className="spinner"></div> {/* Customized spinner */}
       <p>Loading...</p>
+      <div className="progress-bar-container">
+        <div className="progress-bar"></div>
+      </div>
     </div>
   );
 };
